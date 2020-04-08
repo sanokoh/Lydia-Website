9f5a9fb334bad12edcc8e9311d418af0627a88d1
Sano Kohei <nishiki.drop@gmail.com>
2020-04-08 20:25:19 +0900

    最初のアップロード

---
+++ gallery.php
@@ -0,0 +1,406 @@
+<!doctype html>
+<html class="no-js" lang="jp">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Lydia作品集</title>
+    <meta name="description" content="Lydia作品実績をご紹介。こだわりの生地で作られた唯一無二のスーツを御覧ください。">
+    <meta name="keywords" content="新宿,歌舞伎町,オーダーメイドスーツ,派手,生地,画像,ホスト">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/gijgo.css">
+    <link rel="stylesheet" href="css/slicknav.css">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+    <link rel="stylesheet" type="text/css" href="fonts/flaticon.css">
+    <link rel="stylesheet" type="text/css" href="gallery01/lightbox/jquery.lightbox-0.5.css"/>
+
+<style type="text/css">
+/*--- CSSは設置ページ合わせて自由に編集ください --*/
+/*---------------------------------
+	        Base CSS
+---------------------------------*/
+body,ul{
+	margin:0;padding:0;list-style:none;
+}
+img{border:0}
+/* clearfix(削除不可) */
+.clearfix:after { content:"."; display:block; clear:both; height:0; visibility:hidden; }
+.clearfix { display:inline-block; }
+/* for macIE \*/
+* html .clearfix { height:1%; }
+.clearfix { display:block; }
+
+#gallery_wrap {
+	/* width:600px;	margin:0 auto; */
+}
+#gallery_list li{
+	/* width:180px; */
+	/* height:150px; */
+	border:1px solid #ccc;
+	float:left;
+	margin:0 5px 5px 0;
+	overflow:hidden;
+	padding:5px;
+	text-align:center;
+	font-size:12px;
+}
+#gallery_list a.photo{
+	width:100%;
+	height:auto;
+	margin:0 auto;
+	overflow:hidden;
+	display:block;
+}
+/*---------------------------------
+	       /Base CSS
+---------------------------------*/
+
+/*---------------------------------
+	      Pager style
+---------------------------------*/
+.pager_link{
+	text-align:right;
+	padding:10px;
+}
+/*ページャーボタン*/
+.pager_link a {
+    border: 1px solid #aaa;
+    border-radius: 5px 5px 5px 5px;
+    color: #333;
+    font-size: 12px;
+    padding: 3px 7px 2px;
+    text-decoration: none;
+	margin:0 1px;
+}
+/*現在のページ、オーバーボタン*/
+.pager_link a.current,.pager_link a:hover{
+    background: #999;
+    color: #FFFFFF;
+}
+.overPagerPattern{
+	padding:0 2px ;
+}
+/*---------------------------------
+	      /Pager style
+---------------------------------*/
+</style>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript" src="gallery01/lightbox/jquery.lightbox-0.5.min.js"></script>
+<script type="text/javascript">
+$(function() {
+	$('#gallery_list a.photo').lightBox();//lightbox
+});
+</script>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-149358813-2"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'UA-149358813-2');
+    </script>
+
+
+</head>
+
+<body>
+    <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]-->
+
+    <!-- header-start -->
+    <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area">
+                <div class="container-fluid p-0">
+                    <div class="row align-items-center justify-content-between no-gutters">
+                        <div class="col-xl-2 col-lg-2">
+                            <div class="logo-img">
+                                <a href="index.html">
+                                    <!-- <img src="img/logo.png" alt=""> -->
+                                    <h2>LYDIA</h2>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-xl-7 col-lg-8">
+                            <div class="main-menu  d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a href="index.html">Home</a></li>
+                                        <li><a href="about.html">About</a></li>
+                                        <li><a href="service.html">LYDIA Suit</a></li>
+                                        <li><a href="studio.html">LYDIA Studio</a></li>
+                                        <li><a class="active" href="#">Gallery
+                                          <i class="ti-angle-down"></i>
+                                        </a>
+                                          <ul class="submenu">
+                                            <li><a href="gallery.php">作品実績</a></li>
+                                            <li><a href="material.html">生地</a></li>
+                                            <li><a href="model.html">モデル</a></li>
+                                          </ul>
+                                        </li>
+                                        <!-- <li><a href="#">サービス<i class="ti-angle-down"></i></a>
+                                            <ul class="submenu">
+                                                <li><a href="blog.html">出張サービス</a></li>
+                                                <li><a href="single-blog.html">オーダー手順</a></li>
+                                            </ul>
+                                        </li> -->
+                                        <li><a href="index.php">お問い合わせ</a></li>
+                                    </ul>
+                                </nav>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-lg-2 d-none d-lg-block">
+                            <div class="social_media_links">
+                                <ul>
+                                    <!-- <li><a href="#"> <i class="fa fa-facebook"></i> </a></li> -->
+                                    <!-- <li><a href="#"> <i class="fa fa-twitter"></i> </a></li> -->
+                                    <li><a href="https://www.instagram.com/odasutsulydia/"> <i class="fa fa-instagram"></i> </a></li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mobile_menu d-block d-lg-none"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header-end -->
+
+    <!-- slider_area_start -->
+    <div class="slider_area ">
+        <div class="slider_area_inner d-flex align-items-center gallery_bg_3">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-7">
+                        <div class="single_slider">
+                            <div class="slider_text">
+                              <p class="mb-1"></p>
+                                <h3>LYDIA作品集</h3>
+                                <p class="name" ></p>
+                                <a href="material.html" class="boxed-btn4 ">こだわりの生地はこちら</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- slider_area_end -->
+
+    <div class="gallery_area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <div class="section_title text-center mb-75">
+                        <h3>Photo Gallery</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="gallery_wrap" class="container">
+        <?php
+        //----------------------------------------------------------------------
+        // 設定ファイルの読み込みとページ独自設定　※必要に応じて変更下さい(START)
+        //----------------------------------------------------------------------
+        include_once("gallery01/gallery/config.php");//設定ファイルインクルード
+        $img_updir = "gallery01/gallery/upimg";//画像の保存先相対パス
+
+        //埋め込み設置するページの文字コード
+        //Shift-jisは「SJIS」、EUC-JPは「EUC-JP」と指定してください。デフォルトはUTF-8。
+        $encodingType = 'UTF-8';
+        //----------------------------------------------------------------------
+        // 設定ファイルの読み込みとページ独自設定　※必要に応じて変更下さい(END)
+        //----------------------------------------------------------------------
+        	$lines = newsListSortUser(file($file_path),$copyright);//ファイル内容を取得
+        	if(!function_exists('PHPkoubou')){ echo $warningMesse; exit;}else{
+        	$pager = pagerOut($lines,$pagelength,$pagerDispLength);//ページャーを起動する
+        ?>
+        <div class="pager_link"><?php echo $pager['pager_res'];?></div>
+        <ul id="gallery_list" class="clearfix">
+        <?php
+        for($i = $pager['index']; ($i-$pager['index']) < $pagelength; $i++){
+          if(!empty($lines[$i])){
+        	$lines_array[$i] = explode(",",$lines[$i]);
+        	$lines_array[$i][3] = rtrim($lines_array[$i][3]);
+        	//$lines_array[$i][1] = ymd2format($lines_array[$i][1]);//日付フォーマットの適用
+        	if($encodingType!='UTF-8') $lines_array[$i][1]=mb_convert_encoding($lines_array[$i][1],"$encodingType",'UTF-8');
+        	if($encodingType!='UTF-8') $lines_array[$i][2]=mb_convert_encoding($lines_array[$i][2],"$encodingType",'UTF-8');
+        	$alt_text = str_replace('<br />','',$lines_array[$i][2]);
+
+        //ギャラリー表示部（HTML部は自由に変更可）※デフォルトはサムネイルを表示。imgタグの「 thumb_ 」を取れば元画像を表示
+        echo <<<EOF
+        <li>
+            <a class="photo" href="{$img_updir}/{$lines_array[$i][0]}.{$lines_array[$i][3]}" title="{$lines_array[$i][1]}<br />{$lines_array[$i][2]}">
+                <img src="{$img_updir}/thumb_{$lines_array[$i][0]}.{$lines_array[$i][3]}" alt="{$alt_text}" height="135" title="{$alt_text}" />
+            </a>
+        </li>
+        EOF;
+          }
+        }
+        ?>
+        </ul>
+        <div class="pager_link"><?php echo $pager['pager_res'];?></div>
+        <?php PHPkoubou($encodingType,$copyright,$warningMesse);}//著作権表記削除不可?>
+        </div>
+    <div class="appointment_area">
+        <div class="container">
+            <div class="row align-items-center ">
+                <div class="col-xl-12 col-md-12 col-md-12 col-lg-12">
+                    <div class="appointment_info text-center section_title_white">
+                        <h3>お問い合わせ</h3>
+                        <p>お気軽にお問い合わせください。</p>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                      <div class="btn boxed-btn">
+                        <a href="#">Contact</a>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- footer_start -->
+    <footer class="footer">
+        <div class="footer_top bg-img-black">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-md-6 col-lg-3">
+                        <div class="footer_widget">
+                            <div class="footer_logo">
+                                <a href="index.html">
+                                    <!-- <img src="img/logo.png" alt=""> -->
+                                    <h2 class="text-white">LYDIA</h2>
+                                </a>
+                            </div>
+                            <p class="footer_text">東京都新宿区歌舞伎町2丁目27-12<br>
+                              <a class="text-light" href="tel: 03-6302-1938">03-6302-1938</a><br>
+                                <a class="domain" href="mailto:info@lydia-tokyo.com">info@lydia-tokyo.com</a></p>
+                            <div class="socail_links">
+                                <ul>
+                                    <!-- <li>
+                                        <a href="#">
+                                            <i class="fa fa-facebook"></i>
+                                        </a>
+                                    </li> -->
+                                    <!-- <li>
+                                        <a href="#">
+                                            <i class="fa fa-twitter"></i>
+                                        </a>
+                                    </li> -->
+                                    <li>
+                                        <a href="https://www.instagram.com/odasutsulydia/">
+                                            <i class="fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6 col-lg-2">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                    Open Hours
+                            </h3>
+                            <ul>
+                              <li class="text-white">月曜13:00 - 22:00</li>
+                              <li class="text-white"><span>火曜</span><span>13:00 - 22:00</span></li>
+                              <li class="text-white"><span>水曜</span><span>13:00 - 22:00</span></li>
+                              <li class="text-white"><span>木曜</span><span>13:00 - 22:00</span></li>
+                              <li class="text-white"><span>金曜</span><span>13:00 - 22:00</span></li>
+                              <li class="text-white"><span>土曜</span><span>13:00 - 22:00</span></li>
+                              <li class="text-white"><span>日曜</span><span>13:00 - 22:00</span></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-6 col-lg-2">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                  Links
+                            </h3>
+                            <ul>
+                                <li><a href="about.html">About</a></li>
+                                <li><a href="https://www.instagram.com/odasutsulydia/">Instagram</a></li>
+                                <li><a href="index.php"> Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copy-right_text">
+            <div class="container">
+                <div class="footer_border"></div>
+                <div class="row">
+                    <div class="col-xl-12">
+                        <p class="copy_right text-center">Copyright &copy;<script>document.write(new Date().getFullYear());</script>Lydia All rights reserved
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer_end -->
+
+    <!-- JS here -->
+    <script src="js/vendor/modernizr-3.5.0.min.js"></script>
+    <!-- <script src="js/vendor/jquery-1.12.4.min.js"></script> -->
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/isotope.pkgd.min.js"></script>
+    <script src="js/ajax-form.js"></script>
+    <script src="js/waypoints.min.js"></script>
+    <script src="js/jquery.counterup.min.js"></script>
+    <script src="js/imagesloaded.pkgd.min.js"></script>
+    <script src="js/scrollIt.js"></script>
+    <script src="js/jquery.scrollUp.min.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/nice-select.min.js"></script>
+    <script src="js/gijgo.min.js"></script>
+    <script src="js/jquery.slicknav.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/plugins.js"></script>
+
+    <!--contact js-->
+    <script src="js/contact.js"></script>
+    <script src="js/jquery.ajaxchimp.min.js"></script>
+    <script src="js/jquery.form.js"></script>
+    <script src="js/jquery.validate.min.js"></script>
+    <script src="js/mail-script.js"></script>
+    <script src="js/main.js"></script>
+    <script>
+            $('#datepicker').datepicker({
+                iconsLibrary: 'fontawesome',
+                // disableDaysOfWeek: [0, 0],
+            //     icons: {
+            //      rightIcon: '<span class="fa fa-caret-down"></span>'
+            //  }
+            });
+            $('#datepicker2').datepicker({
+                iconsLibrary: 'fontawesome',
+                icons: {
+                 rightIcon: '<span class="fa fa-caret-down"></span>'
+             }
+
+            });
+            var timepicker = $('#timepicker').timepicker({
+             format: 'HH.MM'
+         });
+        </script>
+</body>
+</html>
